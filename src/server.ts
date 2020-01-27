8d0ff082f0ada8098997162568cc15d0f17eb813
Flavio Vinicius Miranda <vinniimiranda@gmail.com>
2020-01-27 00:09:21 -0300

    Port added

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import Socket from 'socket.io';
 
 import { getActions } from './index';
 
+const PORT = process.env.PROT || 3333;
+
 const server = createServer();
 const io = Socket(server);
 
@@ -14,4 +16,4 @@ setInterval(async () => {
   io.emit('getData', await getActions());
 }, 6000);
 
-export default server.listen(3333);
+export default server.listen(PORT);
